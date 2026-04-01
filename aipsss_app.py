ba866ae4772386b52b961efe42fff91add3967ec
kannankarur04-droid <kannankarur04@gmail.com>
2026-04-01 15:44:21 +0530

    aipsss_app.py

--- aipsss_app.py
+++ aipsss_app.py
@@ -41,7 +41,7 @@ st.markdown("""
 
     /* Smaller Logo */
     main-logo {
-        width: 160px !important; /* 100px லிருந்து 160px ஆக உயர்த்தப்பட்டுள்ளது */
+        width: 100px !important; /* 100px லிருந்து 160px ஆக உயர்த்தப்பட்டுள்ளது */
         height: auto;
         object-fit: contain;
     }
